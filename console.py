3dc2babbf50dc5209744d3eb3ab10d37deee75f2
Gertsev <gertsev.mikl@gmail.com>
2019-02-25 19:19:12 +0300

    console fix

--- console.py
+++ console.py
@@ -3,54 +3,54 @@ import os
 import argparse
 
 def tree(path, mask=''):
-	dir = os.listdir(path)
-	j = 0
-	while j < len(dir):
+    dir = os.listdir(path)
+    j = 0
+    while j < len(dir):
 
-		if dir[j][0]=='.' and args.all:
-			dir.pop(j)
+        if dir[j][0]=='.' and args.all:
+            dir.pop(j)
 
-		elif os.path.isfile(os.path.join(path,dir[j])):
-			if args.folders_only:
-				dir.pop(j)
-			elif args.include is not None and dir[j].find(args.include)==-1:
-				dir.pop(j)
-			elif args.exclude is not None and dir[j].find(args.exclude)!=-1:
-				dir.pop(j)
-			else:
-				j += 1
+        elif os.path.isfile(os.path.join(path,dir[j])):
+            if args.folders_only:
+                dir.pop(j)
+            elif args.include is not None and dir[j].find(args.include)==-1:
+                dir.pop(j)
+            elif args.exclude is not None and dir[j].find(args.exclude)!=-1:
+                dir.pop(j)
+            else:
+                j += 1
 
-		else:
-			j +=1
+        else:
+            j +=1
 
-	for j in range(len(dir)-1):
-		if args.full:
-			name = os.path.join(path,dir[j])
-		else:
-			name = dir[j]
+    for j in range(len(dir)-1):
+        if args.full:
+            name = os.path.join(path,dir[j])
+        else:
+            name = dir[j]
 
-		if args.pretty:
-			print(mask, '├─ ', name, sep='')
-		else:
-			print(mask, '   ', name, sep='')
-		if os.path.isdir(os.path.join(path,dir[j])):
-			if args.pretty:
-				tree(os.path.join(path,dir[j]), mask + '│  ')
-			else:
-				tree(os.path.join(path,dir[j]), mask + '   ')
-	j = len(dir) - 1
-	if j<0:
-		return
-	if args.full:
-		name = os.path.join(path,dir[j])
-	else:
-		name = dir[j]	
-	if args.pretty:
-		print(mask, '└─ ', name, sep='')
-	else:
-		print(mask, '   ', name, sep='')
-	if os.path.isdir(os.path.join(path,dir[j])):
-		tree(os.path.join(path,dir[j]),mask + '   ')
+        if args.pretty:
+            print(mask, '├─ ', name, sep='')
+        else:
+            print(mask, '   ', name, sep='')
+        if os.path.isdir(os.path.join(path,dir[j])):
+            if args.pretty:
+                tree(os.path.join(path,dir[j]), mask + '│  ')
+            else:
+                tree(os.path.join(path,dir[j]), mask + '   ')
+    j = len(dir) - 1
+    if j < 0:
+        return
+    if args.full:
+        name = os.path.join(path,dir[j])
+    else:
+        name = dir[j]	
+    if args.pretty:
+        print(mask, '└─ ', name, sep='')
+    else:
+        print(mask, '   ', name, sep='')
+    if os.path.isdir(os.path.join(path,dir[j])):
+        tree(os.path.join(path,dir[j]),mask + '   ')
 
 parser = argparse.ArgumentParser(
     description='Статистика для самых маленьких'
@@ -96,7 +96,7 @@ parser.add_argument(
 parser.add_argument(
     '-I',
     '--include',
-	metavar='SOME_TEXT',
+    metavar='SOME_TEXT',
     type=str,
     action='store',
     help='выводить только имена, содержащие SOME_TEXT'
@@ -105,7 +105,7 @@ parser.add_argument(
 parser.add_argument(
     '-E',
     '--exclude',
-	metavar='SOME_TEXT',
+    metavar='SOME_TEXT',
     type=str,
     action='store',
     help='не выводить имена, содержащие SOME_TEXT'
